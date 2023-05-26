bdd4b3132b3e0b911fa42bcdf4b64410c7c0fd74
AntonSidorin <antonsidorin@list.ru>
2023-05-26 06:10:08 +0300

    Update gitignore

---
+++ Programming_languages/Java/2.Basics/README.md
@@ -0,0 +1,16 @@
+# Java :coffee:
+
+## 1. Java from CLI (1.Manual_run)
+
+Here we will run our first java program from terminal
+
+## 2. Java basics (2.Basics)
+
+There are:
+
+- variables
+- types
+- loops
+- conditions
+- methods (functions)
+- ...
---
+++ Programming_languages/Java/2.Basics/src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+
+    int intVar = 10;
+    double doubleVar = 10.1;
+    String stringVar = "10";
+
+    int emptyIntVar;
+    emptyIntVar = 100;
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
